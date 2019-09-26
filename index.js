9f97b3751743cfc8b1658b67dc664c729ad099df
Anthony Rurup <tcrurup@gmail.com>
2019-09-26 20:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Code your solution in this file!
 
-function returnFirstTwoDrivers(drivers){
-  
-  const myFunc = function(drivers){
+const returnFirstTwoDrivers = function(drivers){
     return drivers.slice(0, 2);
-  };
-  
-  return myFunc();
-}
+};
